45582cee1a60c4170995d84e68145cbd26bd8591
mugambi645 <mugambi645@gmail.com>
2020-03-12 20:17:39 +0300

    first changes

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function checkAddress(fieldId) {
+  var val=document.getElementById(fieldId).value;
+  if val === ""(
+    alert("email address required");
+  )
+}
+function submitEmail(email) {
+  var val=document.getElementById(email).value;
+  if email === ""(
+    alert("submitted succesfully")
+  )
+}
